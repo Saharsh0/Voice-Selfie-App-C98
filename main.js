3b6481b3de308bc2a94b59ee65bad71ef675553c
Saharsh0 <113522933+Saharsh0@users.noreply.github.com>
2023-03-22 17:44:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,19 +16,29 @@ recognition.onresult = function(event)
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
+    if(Content =="take my selfie")
+   {
+    console.log("taking selfie ---")
     speak();
+   }
 }
 
 function speak()
 {
     var synth = window.speechSynthesis;
     
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking selfie in 5 seconds"
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save()
+    }, 5000);
 }
 
 Webcam.set({
@@ -38,3 +48,19 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">'
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
